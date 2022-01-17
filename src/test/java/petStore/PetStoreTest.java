70d8fa15bb89f785f020fd4debe323d2b31f7b73
MarceloMansilla <mansillamarcelo90@gmail.com>
2022-01-16 22:27:16 -0300

    Implementing parallel execution

--- src/test/java/petStore/PetStoreTest.java
+++ src/test/java/petStore/PetStoreTest.java
@@ -1,5 +1,9 @@
 package petStore;
 
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit5.Karate;
 /*import com.intuit.karate.Results;
@@ -7,20 +11,15 @@ import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;*/
 
-@KarateOptions(tags = {"@regression",""})
+@KarateOptions(tags = {"@regression","@user","@pets"})
 class PetStoreTest {
 
-    @Karate.Test
-    Karate testUsers() {
-        return Karate.run().relativeTo(getClass());
-    }    
 
-    /*@Test
+    @Test
     void testParallel() {
         Results results = Runner.path("classpath:petStore")
                 //.outputCucumberJson(true)
-                .parallel(5);
+                .parallel(10);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
-    }*/
-
+    }
 }
